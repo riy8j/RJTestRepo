280c4ccf54cb6f7b0269a288a49f0ac86b956701
Michael Petrone <224322441+MPCode-Dev@users.noreply.github.com>
2026-01-14 16:41:59 -0500

    added comment to hello world, removed michael from file name

--- HelloWorldMichael.java
+++
@@ -1,6 +0,0 @@
-
-Public class HelloWorld {
-	public static void main (String[] args){
-		System.out.println("Hello, World!");
-	}
-}
--- helloWorld.java
+++ helloWorld.java
@@ -1,1 +1,7 @@
 
+Public class HelloWorld {
+	public static void main (String[] args){
+		// Say, "Hello World"
+		System.out.println("Hello, World!");
+	}
+}
